e8ad32ccdfa7af710758ed5512db596783a8c6ef
mr_bushenyula <bushenulatheophile@gmail.com>
2023-03-02 13:39:56 +0000

    day four of 100days of code

---
+++ dayFour.py
@@ -0,0 +1,3 @@
+name= "theophile"
+print(""" today I have learnt to print my name in color
+for example now i am going to print it in """ , "\033[31m", "red we have", name,"\033[0m", "the the next one i am going to make it ", "\033[32m", "green","\033[0m","and the rest will be","\033[34m","blue like that")
